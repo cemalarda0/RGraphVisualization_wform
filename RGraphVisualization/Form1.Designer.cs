7a4ec90970621fb95e9df5b5feae52f62f27ce35
cemalarda0 <66903746+cemalarda0@users.noreply.github.com>
2024-11-10 19:30:58 +0300

    Add files via upload

---
+++ RGraphVisualization/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace RGraphVisualization
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.graphPicture = new System.Windows.Forms.PictureBox();
+            this.generateGraph = new System.Windows.Forms.Button();
+            this.addFile = new System.Windows.Forms.Button();
+            this.graphTypeText = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.graphPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graphPicture
+            // 
+            this.graphPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.graphPicture, "graphPicture");
+            this.graphPicture.Name = "graphPicture";
+            this.graphPicture.TabStop = false;
+            // 
+            // generateGraph
+            // 
+            this.generateGraph.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.generateGraph, "generateGraph");
+            this.generateGraph.Name = "generateGraph";
+            this.generateGraph.UseVisualStyleBackColor = false;
+            // 
+            // addFile
+            // 
+            this.addFile.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.addFile, "addFile");
+            this.addFile.Name = "addFile";
+            this.addFile.UseVisualStyleBackColor = false;
+            this.addFile.Click += new System.EventHandler(this.addFile_Click);
+            // 
+            // graphTypeText
+            // 
+            resources.ApplyResources(this.graphTypeText, "graphTypeText");
+            this.graphTypeText.Name = "graphTypeText";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Name = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1")});
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Tag = "";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.graphTypeText);
+            this.Controls.Add(this.addFile);
+            this.Controls.Add(this.generateGraph);
+            this.Controls.Add(this.graphPicture);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.graphPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox graphPicture;
+        private System.Windows.Forms.Button generateGraph;
+        private System.Windows.Forms.Button addFile;
+        private System.Windows.Forms.Label graphTypeText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ RGraphVisualization/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGraphVisualization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string folderPath = Path.Combine(Application.StartupPath, "RGraph");
+
+            if (Directory.Exists(folderPath))
+            {
+                string[] files = Directory.GetFiles(folderPath);
+
+                foreach (string file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    comboBox1.Items.Add(fileName);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Belirtilen klasör bulunamadı: " + folderPath);
+            }
+        }
+
+        private void addFile_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+
+            // Seçilen klasörün yolunu al
+            string selectedPath = folderDialog.SelectedPath;
+
+            // Klasördeki dosyaları al ve ilk dosyanın ismini Label'a ekle
+            string[] files = Directory.GetFiles(selectedPath);
+            label1.Text = (folderDialog.ShowDialog() == DialogResult.OK) ? Path.GetFileName(files[0]) : "Dosya bulunamadı."; // Klasörde dosya yoksa
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ClearDynamicControls();
+
+            if (comboBox1.SelectedItem.ToString() == "Seçenek 1")
+            {
+                // Buton ekleyin
+                Button button = new Button();
+                button.Text = "Dinamik Buton";
+                button.Location = new Point(50, 100);
+                button.Click += new EventHandler(DynamicButton_Click); // Buton tıklama olayı
+                this.Controls.Add(button);
+
+                // RadioButton ekleyin
+                RadioButton radio1 = new RadioButton();
+                radio1.Text = "Radio 1";
+                radio1.Location = new Point(50, 150);
+                this.Controls.Add(radio1);
+
+                RadioButton radio2 = new RadioButton();
+                radio2.Text = "Radio 2";
+                radio2.Location = new Point(50, 180);
+                this.Controls.Add(radio2);
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Seçenek 2")
+            {
+                // Farklı bir seçim için farklı buton ve radiolar ekleyin
+                Button button2 = new Button();
+                button2.Text = "Başka Bir Buton";
+                button2.Location = new Point(50, 100);
+                button2.Click += new EventHandler(DynamicButton_Click);
+                this.Controls.Add(button2);
+
+                RadioButton radio3 = new RadioButton();
+                radio3.Text = "Radio 3";
+                radio3.Location = new Point(50, 150);
+                this.Controls.Add(radio3);
+            }
+        }
+
+        private void DynamicButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dinamik buton tıklandı!");
+        }
+
+        private void ClearDynamicControls()
+        {
+            // Formdaki geçici kontrolleri temizleyin
+            foreach (Control ctrl in this.Controls.OfType<Button>().ToList())
+            {
+                this.Controls.Remove(ctrl);
+                ctrl.Dispose(); // Kontrolü hafızadan serbest bırakın
+            }
+
+            foreach (Control ctrl in this.Controls.OfType<RadioButton>().ToList())
+            {
+                this.Controls.Remove(ctrl);
+                ctrl.Dispose(); // Kontrolü hafızadan serbest bırakın
+            }
+        }
+    }
+}
